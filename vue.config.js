8826089e523ed16f9e599da8816780dc41fa5d5a
Giovanne <giovanneafonso@gmail.com>
2019-06-29 12:26:21 -0400

    Update build public path

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ module.exports = {
 
   // Build this project to /docs folder
   // so I can publish it on github pages
-  outputDir: 'docs'
+  outputDir: 'docs',
+
+  // Relative public path on build
+  // because ghpages publish code to a subdomain
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
 
 }
